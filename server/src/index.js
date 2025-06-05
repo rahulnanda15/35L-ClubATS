dd5c634aae512108163dda3b2e91465175cc7104
Jishan Kharbanda <jishankharbanda@gmail.com>
2025-06-04 17:19:07 -0700

    Updated settings to enable profile editing with live update, email uniqueness check, and frontend success/error feedback

--- server/src/index.js
+++ server/src/index.js
@@ -8,7 +8,7 @@ import filesRoutes from './routes/files.js';
 import authRoutes from './routes/auth.js';
 
 import config from './config.js';
-
+import adminRoutes from './routes/admin.js';
 const app = express();
 
 app.use(cors());
@@ -18,6 +18,7 @@ app.use(express.json());
 app.use('/api/auth', authRoutes);
 app.use('/api/applications', applicationsRoutes);
 app.use('/api/files', filesRoutes);
+app.use('/api/admin', adminRoutes);
 
 await syncFormResponses();
 cron.schedule('*/5 * * * *', () => {
